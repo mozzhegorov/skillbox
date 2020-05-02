d6546cf4d7e02abd84007e4d61232db03e9708fa
denis <denislegrace@gmail.com>
2020-05-02 14:36:14 +0300

    little changes in mail

--- php/mail.php
+++ php/mail.php
@@ -38,7 +38,7 @@ if ( $method === 'POST' ) {
 	}
 }
 
-$message = "<table style='width: 50%;'>ПОступила заявка $message</table>";
+$message = "Поступила заявка <table style='width: 50%;'> $message</table>";
 
 function adopt($text) {
 	return '=?UTF-8?B?'.Base64_encode($text).'?=';
@@ -49,4 +49,4 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-mail($admin_email, adopt($form_subject), $message, $headers, );
+mail($admin_email, adopt($form_subject), $message, $headers);
